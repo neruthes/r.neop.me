3c1a6fb4d8b41ee70ecc588efed6a0c4d1a26188
Neruthes (a.k.a. J.N.) <i@neruthes.xyz>
2019-05-20 11:42:34 +0000

    hello world

---
+++ add.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+var argv = process.argv;
+
+if (argv.length !== 3) {
+    console.error('Please give 1 and only 1 URL.');
+} else {
+    var url = argv[2];
+    if (url.indexOf('://') === -1) {
+        url = 'https://' + url;
+    };
+
+    // Read
+    var latestId = parseInt(fs.readFileSync('latest-id.txt').toString().trim(), 36);
+    var currentId = latestId + 1;
+    var currentId_base36 = currentId.toString(36);
+
+    // Write
+    fs.writeFileSync(`db/${currentId_base36}.txt`, url);
+    fs.writeFileSync(`latest-id.txt`, currentId_base36);
+
+    // Log
+    console.log('Successful!');
+    console.log(`Now /${currentId_base36}`);
+    console.log(`Will be redirected to ${url}`);
+};
---
+++ db/1.txt
@@ -0,0 +1,1 @@
+https://neruthes.xyz/
