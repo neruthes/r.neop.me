5c41c25c6758722b2f71c829664f484eacb0bda3
Neruthes (a.k.a. J.N.) <i@neruthes.xyz>
2019-05-30 04:00:28 +0000

    Regular update: 2019-5-30 04:00:28 UTC

--- add.js
+++ add.js
@@ -1,14 +1,24 @@
 const exec = require('child_process').exec;
+const crypto = require('crypto');
+const CryptoJS = require('crypto-js');
 const fs = require('fs');
 
 var argv = process.argv;
 
-if (argv.length !== 3) {
+if (argv.length < 3 || argv.length > 4) {
     console.error('Please give 1 and only 1 URL.');
 } else {
     var url = argv[2];
     if (url.indexOf('://') === -1) {
         url = 'https://' + url;
+    };
+    if (argv[3] !== undefined) {
+        // Given AES key
+        var aeskey = argv[3];
+        if (!aeskey.match(/^[0-9A-Za-z]+$/)) {
+            console.error('Bad AES key. Use only A-Z, a-z, and 0-9.')
+        };
+        url = CryptoJS.AES.encrypt(url, aeskey);
     };
 
     // Read
@@ -22,7 +32,7 @@ if (argv.length !== 3) {
 
     // Log
     console.log('Successful!');
-    console.log(`Now https://udon.pw/${currentId_base36}`);
+    console.log(`Now https://udon.pw/${currentId_base36}${aeskey ? '#' + aeskey : ''}`);
     console.log(`Will be redirected to ${url}`);
 
     // Auto push
